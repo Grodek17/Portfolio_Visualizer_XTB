9ac76a634d8fe1623e40790e20716356d2710ca8
Mateusz <grodek7483@gmail.com>
2026-08-24 12:44:04 +0200

    started work on benchmark portfolio comparison

--- benchmark_comparison.py
+++ benchmark_comparison.py
@@ -4,8 +4,7 @@ import matplotlib.pyplot as plt
 import yfinance as yf
 import sys
 
-from constants import URL
-from dictionary import XTB_TO_YAHOO, TICKER_EXCEPTIONS, XTB_TO_CURRENCY, CURRENCY_TICKERS
+from dictionary import CURRENCY_TICKERS, BENCHMARK_XTB_TICKERS
 
 from asset_class import Asset
 
@@ -39,8 +38,6 @@ class Position:
     def getAvgPrice(self):
         return self.avg_price
 
-    def getCurrency(self):
-        return self.currency
 
 
 def read_cash_operations(url):
@@ -65,7 +62,7 @@ def parse_open_transaction(comment):
 
 #helper function, reads all transaction from given day in dataframe, updates stocks volumes and average prices[different currencies], and total money invested [pln]
 def read_all_transactions_from_this_day(today_transactions, positions, total_dividends, total_invested):
-
+    invested_today = 0
     dividend_types = [
         "Dividend",
         "Dividend from foreign company on PL market",
@@ -87,12 +84,13 @@ def read_all_transactions_from_this_day(today_transactions, positions, total_div
             volume, price = parse_open_transaction(comment)
             positions[ticker].add_pucharse(volume, price)
             total_invested += (amount * (-1))
+            invested_today += (amount * (-1))
     
         
         if type in dividend_types:
             total_dividends += amount
 
-    return positions, total_dividends, total_invested
+    return positions, total_dividends, total_invested, invested_today
 
 
 #helper function, gets currency value for each day
@@ -180,7 +178,20 @@ def plot_benchmarks(portfolio_returns):
     fig.autofmt_xdate()
     plt.tight_layout()
     plt.show()
-    
+
+
+#returns {ticker : asset_class} disctionary for benchmarks like SP500
+def get_benchmarks_data(yahoo_tickers_list, start_date, end_date):
+    start_date = start_date.strftime('%Y-%m-%d') 
+    end_date = end_date.strftime('%Y-%m-%d') 
+    benchmark_data_dict = {}
+    benchmark_position_dict = {}
+    for ticker in yahoo_tickers_list:
+        benchmark_data_dict[ticker] = Asset(ticker, start_date, end_date)
+        benchmark_position_dict[ticker] = Position(ticker)
+
+    return benchmark_data_dict, benchmark_position_dict
+
     
 #download all data across whole "benchmarking time frame", for easier and quicker access in the future
 def get_yahoo_price_data(transactions, start_date, end_date):
@@ -229,12 +240,12 @@ def download_yf_exchange_rates(ticker, startdate, enddate, data_interval="1d"):
     return YahooDF
     
 #
-def get_exchange_rates_data(start_date, end_date, assets):
+def get_exchange_rates_data(start_date, end_date, positions_data):
     start_date = start_date.strftime('%Y-%m-%d') 
     end_date = end_date.strftime('%Y-%m-%d')
     currencies = []
     exchange_rates = {}
-    for ticker, asset in assets.items():
+    for ticker, asset in positions_data.items():
         currencies.append(asset.get_currency())
 
     currencies = list(set(currencies))
@@ -253,6 +264,21 @@ def get_exchange_rates_data(start_date, end_date, assets):
     return exchange_rates
 
 
+def update_benchmark_positions(invested_today, benchmarks_positions, benchmarks_data, day, exchange_rates):
+    for ticker, key in benchmarks_positions.items():
+        #get position price for the day
+        position_price = benchmarks_data[ticker].get_price_of_day(day)
+        position_currency = benchmarks_data[ticker].get_currency()
+        exchange_rate = exchange_rates[position_currency].loc[day]
+
+        #calculate bought volume
+        bought_volume = (invested_today/exchange_rate)/position_price
+
+        #update benchmark volume and average price
+        benchmarks_positions[ticker].add_pucharse(bought_volume, position_price)
+
+
+    return benchmarks_positions
 
 
 #compares your pucharses with theoretical parallel benchmmark pucharses (e.g. SP500, NASDAQ100) to check if you are "beating" popular ETF's with your picks
@@ -260,9 +286,11 @@ def get_exchange_rates_data(start_date, end_date, assets):
 # to be settled from a PLN-denominated account.
 def portfolio_benchmark(url):
     positions = {}                                                  #stores {xtb ticker : class storing (volume, avg_price, currency)}
-    assets = {}                                                     #stores {xtb_ticker : class storing asset info (name, ticker, dataframe with historical prices)}
+    benchmarks_positions = {}                                       #stores {yahoo_ticker : class storing (volume, avg_price, currency)}
+    positions_data = {}                                             #stores {xtb_ticker : class storing asset info (name, ticker, dataframe with historical prices)}
+    benchmarks_data = {}                                            #stores {yahoo_ticker : class storing asset info}
     exchange_rates = {}                                             #stores {currency_ticker : exchange_rate series}
-    daily_returns = {}
+    daily_returns = {}                                              #stores {day : portfolio value}
     total_dividends = 0
     total_invested = 0
 
@@ -271,36 +299,39 @@ def portfolio_benchmark(url):
     start_date = pd.to_datetime(df["Time"]).min()
     end_date = pd.Timestamp.today().normalize()
 
-    assets = get_yahoo_price_data(df, start_date, end_date)         #download historical price data & metadata for every ticker in xtb report
-    exchange_rates = get_exchange_rates_data(start_date, end_date, assets)
-    print("==== DEBUG ====")
-    print(exchange_rates.keys())
-    print("==== DEBUG ====")
+    positions_data = get_yahoo_price_data(df, start_date, end_date)         #download historical price data & metadata for every ticker in xtb report
+    benchmarks_data, benchmarks_positions = get_benchmarks_data(BENCHMARK_XTB_TICKERS, start_date, end_date)
+    exchange_rates = get_exchange_rates_data(start_date, end_date, positions_data)
 
     #calculate portfolio value and report changes for every day of an timeframe
     for day in pd.date_range(start=start_date, end=end_date, freq="D"):
+        invested_today = 0
         day = day.strftime('%Y-%m-%d') 
-        print(day)
 
         #get all transactions in current day
         today_transactions = df.loc[df["Time"] == day]
 
         #update all stocks volumes and prices accordingly
-        positions, total_dividends, total_invested = read_all_transactions_from_this_day(today_transactions, positions, total_dividends, total_invested)
+        positions, total_dividends, total_invested, invested_today = read_all_transactions_from_this_day(today_transactions, positions, total_dividends, total_invested)
+
+        #parallely buy benchmarks
+        benchmarks_positions = update_benchmark_positions(invested_today, benchmarks_positions, benchmarks_data, day, exchange_rates)
+
+        #TODO: return rates for benchmarks + adding all data in dict, or list
 
         #at the end of each day, calculate "todays value of portfolio, by summing volumes and todays prices"
         today_portfolio_value = 0
         for ticker, position in positions.items():
             position_volume = position.getVolume()
-            position_currency = assets[ticker].get_currency()
+            position_currency = positions_data[ticker].get_currency()
             position_exchange_rate = exchange_rates[position_currency].loc[day]
-            position_price_most_recent = assets[ticker].get_price_of_day(day)
+            position_price_most_recent = positions_data[ticker].get_price_of_day(day)
 
             position_value_this_day = position_volume * position_price_most_recent * position_exchange_rate
             today_portfolio_value = today_portfolio_value + position_value_this_day
              
         this_day_return_rate_in_percent = (((today_portfolio_value)/(total_invested))-1) 
-        return_rate = round(this_day_return_rate_in_percent, 2)
+        return_rate = this_day_return_rate_in_percent
 
         daily_returns[day] = return_rate
         
